db1c2c49252e3025c270adf735542c396c41d621
Elton Quek <quekmkelton@gmail.com>
2022-12-06 17:55:47 +0800

    View Command

--- src/main/java/com/eltonquek/showbooking/commands/ViewCommand.java
+++ src/main/java/com/eltonquek/showbooking/commands/ViewCommand.java
@@ -1,19 +1,73 @@
 package com.eltonquek.showbooking.commands;
 
+import com.eltonquek.showbooking.SystemMemory;
+import com.eltonquek.showbooking.entities.Booking;
+import com.eltonquek.showbooking.entities.Seat;
+import com.eltonquek.showbooking.entities.Show;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Component
 public class ViewCommand implements Command {
 
+    @Autowired
+    private SystemMemory memory;
+
     @Override
     public boolean validate(String[] inputs) {
 //        View <Show Number>
-        System.out.println("validation~");
-        return false;
+        if (inputs.length != 2 || !Objects.equals(inputs[0], "View")) {
+            return false;
+        }
+
+        try {
+            int showNumber = Integer.parseInt(inputs[1]);
+            if (memory.getShowList().stream().map(Show::getShowNumber).noneMatch(currentShowNumber -> currentShowNumber == showNumber)) {
+                return false;
+            }
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+
+        return true;
     }
 
     @Override
     public void run(String[] inputs) {
-        System.out.println("run view~");
+        // (To display Show Number, Ticket#, Buyer Phone#, Seat Numbers allocated to the buyer)
+        // Ensure that run cannot be used without valid inputs
+        if (!validate(inputs)) {
+            throw new RuntimeException("Invalid input");
+        }
+        int showNumber = Integer.parseInt(inputs[1]);
+        Optional<Show> optionalShow = memory.getShowList().stream().filter(currentShow -> currentShow.getShowNumber() == showNumber).findFirst();
+        if (optionalShow.isPresent()) {
+            Show show = optionalShow.get();
+            System.out.printf("Show number: %d.\n", showNumber);
+
+            if (show.getBookingList().size() == 0) {
+                System.out.println("No bookings have been found for this show");
+            } else {
+                System.out.println("---------------------------------------------------------------------------------------------");
+                System.out.printf("%18s %25s %35s", "Ticket Number", "Buyer Phone Number", "Seat Numbers");
+                System.out.println();
+                System.out.println("---------------------------------------------------------------------------------------------");
+                //iterates over the list
+                for (Booking booking : show.getBookingList()) {
+                    String seatList =  booking.getSeatList().stream().map(Seat::getSeatNumber).collect(Collectors.joining(", "));
+                    System.out.format("%18s %25s %35s", booking.getTicketNumber(),  booking.getPhoneNumber(), seatList);
+                    //TODO: Beautify seat list when seat list is big
+                    System.out.println();
+                }
+                System.out.println("---------------------------------------------------------------------------------------------");
+            }
+        } else {
+            throw new RuntimeException("Show not found");
+        }
     }
 }
--- src/test/java/com/eltonquek/showbooking/commands/ViewCommandTest.java
+++ src/test/java/com/eltonquek/showbooking/commands/ViewCommandTest.java
@@ -1,13 +1,22 @@
 package com.eltonquek.showbooking.commands;
 
 import com.eltonquek.showbooking.SystemMemory;
+import com.eltonquek.showbooking.entities.Booking;
+import com.eltonquek.showbooking.entities.Seat;
 import com.eltonquek.showbooking.entities.Show;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,15 +29,41 @@ public class ViewCommandTest {
     @Autowired
     private SystemMemory memory;
 
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
     @BeforeEach
     void setup() {
-        memory.getShowList().add(new Show(1, 1, 1, 1));
+        Show show1 = new Show(1, 1, 1, 1);
+        List<Seat> seatList=  new ArrayList<>();
+        seatList.add(new Seat("A1"));
+        List<Booking> bookingList=  new ArrayList<>();
+        bookingList.add(new Booking("1234", "1234", seatList));
+        show1.setBookingList(bookingList);
+        memory.getShowList().add(show1);
+
+
+        Show show6 = new Show(6, 1, 1, 1);
+        List<Seat> seatList6=  new ArrayList<>();
+        seatList6.add(new Seat("A2"));
+        seatList6.add(new Seat("B4"));
+        seatList6.add(new Seat("C4"));
+        seatList6.add(new Seat("D5"));
+        List<Booking> bookingList6=  new ArrayList<>();
+        bookingList6.add(new Booking("1234", "1234", seatList6));
+        show6.setBookingList(bookingList6);
+        memory.getShowList().add(show6);
+
         memory.getShowList().add(new Show(2, 1, 1, 1));
+
+        System.setOut(new PrintStream(outContent));
     }
 
     @AfterEach
     void tearDown() {
         memory.getShowList().clear();
+        System.setOut(originalOut);
     }
 
     @ParameterizedTest
@@ -45,12 +80,25 @@ public class ViewCommandTest {
         assertFalse(uut.validate(inputArray));
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"View 1", "View 2"})
-    void run_viewShow(String input) {
-        String[] inputArray = input.split(" ");
+    @Test
+    void run_viewShow1() {
+        String[] inputArray = new String[]{"View", "1"};
         uut.run(inputArray);
-        assertEquals(1, memory.getShowList().size());
+        assertTrue(Objects.equals(show1test().trim(), outContent.toString().trim().replace("\r","")));
+    }
+
+    @Test
+    void run_viewShow2() {
+        String[] inputArray = new String[]{"View", "2"};
+        uut.run(inputArray);
+        assertTrue(Objects.equals(show2test().trim(), outContent.toString().trim().replace("\r","")));
+    }
+
+    @Test
+    void run_viewShow6() {
+        String[] inputArray = new String[]{"View", "6"};
+        uut.run(inputArray);
+        assertTrue(Objects.equals(show6test().trim(), outContent.toString().trim().replace("\r","")));
     }
 
     @ParameterizedTest
@@ -58,5 +106,28 @@ public class ViewCommandTest {
     void run_throwException_invalidInput(String input) {
         String[] inputArray = input.split(" ");
         assertThrows(RuntimeException.class, () -> uut.run(inputArray));
+    }
+
+    private String show1test(){
+        return "Show number: 1.\n" +
+                "---------------------------------------------------------------------------------------------\n" +
+                "     Ticket Number        Buyer Phone Number                        Seat Numbers\n" +
+                "---------------------------------------------------------------------------------------------\n" +
+                "              1234                      1234                                  A1\n" +
+                "---------------------------------------------------------------------------------------------\n";
+    }
+
+    private String show2test(){
+        return "Show number: 2.\n" +
+                "No bookings have been found for this show\n";
+    }
+
+    private String show6test(){
+        return "Show number: 6.\n" +
+                "---------------------------------------------------------------------------------------------\n" +
+                "     Ticket Number        Buyer Phone Number                        Seat Numbers\n" +
+                "---------------------------------------------------------------------------------------------\n" +
+                "              1234                      1234                      A2, B4, C4, D5\n" +
+                "---------------------------------------------------------------------------------------------\n";
     }
 }
